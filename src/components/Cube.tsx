b7c5c39def890b3484b30fe84838c68abba4506f
jannalulu <109004049+jannalulu@users.noreply.github.com>
2024-11-16 16:17:18 -0500

    remove lighthouse, create tsx components

---
+++ src/components/Cube.tsx
@@ -0,0 +1,24 @@
+import { useGLTF } from '@react-three/drei'
+import { GroupProps } from '@react-three/fiber'
+import { GLTF } from 'three-stdlib'
+import * as THREE from 'three'
+
+type GLTFResult = GLTF & {
+  nodes: {
+    cube: THREE.Mesh
+  }
+  materials: {
+    cube: THREE.MeshStandardMaterial
+  }
+}
+
+export function Cube(props: GroupProps) {
+  const { nodes, materials } = useGLTF('/src/components/cube.gltf') as GLTFResult
+  return (
+    <group {...props} dispose={null}>
+      <mesh geometry={nodes.cube.geometry} material={materials.cube} />
+    </group>
+  )
+}
+
+useGLTF.preload('/src/components/cube.gltf')
